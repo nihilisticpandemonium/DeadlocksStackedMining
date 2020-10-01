3505497203437d90c35cf6a457eef16bb6dbd0bd
NihilisticPandemonium <whiro@tukui.org>
2020-10-01 01:05:23 -0700

    Readd the tenemut compatibility changes.

--- prototypes/ores.lua
+++ prototypes/ores.lua
@@ -1,4 +1,4 @@
--- safely get the value of a setting 
+-- safely get the value of a setting
 local function getSettingValue(optionName)
     if settings["startup"] and settings["startup"][optionName] and settings["startup"][optionName].value then
 		return settings["startup"][optionName].value
@@ -10,20 +10,22 @@ end
 local stackSize
 if getSettingValue("kyth-overwrite-deadlock-stack-size") == "disabled" then
     stackSize =  tonumber(getSettingValue("deadlock-stack-size"))
-else 
+else
     stackSize = tonumber(getSettingValue("kyth-overwrite-deadlock-stack-size"))
 end
 
 ---------------------------------------------------------------------------------------------------
 
 -- check if the stacked version of the item with the given name exists and in case it does not, create it
-    -- returns true when an item was created or if it already exists, otherwise false
+-- returns true when an item was created or if it , otherwise false
 local function createStackedVersion(name)
-    if data.raw["item"][name] then 
-        if data.raw["item"]["deadlock-stack-" .. name] then
+    local base = data.raw["item"][name] or data.raw["tool"][name]
+    if base then
+        local item_type = data.raw["item"][name] and "item" or "tool"
+        if data.raw[item_type]["deadlock-stack-" .. name] then
             return true
         else
-            deadlock.add_stack(name)
+            deadlock.add_stack(name, nil, nil, nil, item_type)
             log("Created stacked version of the item " .. name .. ", because it was missing. It is advised to install a mod that adds support for the mod that this item is from in case you did not already")
             return true
         end
@@ -42,7 +44,7 @@ local function createStackedOre(oreName)
 
     -- generate a dynamic localized name for the stacked version of the ore
     ore.localised_name = {"entity-name.stacked-ore",{"entity-name." .. oreName}}
-    
+
     -- replace every result of minable with their stacked version
     if ore.minable.results then
         local tempResults = {}
@@ -67,9 +69,9 @@ local function createStackedOre(oreName)
             end
         end
         ore.minable.results = tempResults
-    elseif ore.minable.result then        
+    elseif ore.minable.result then
         if createStackedVersion(ore.minable.result) then
-            ore.minable.results = 
+            ore.minable.results =
             {{
                 amount_max = ore.minable.count or 1,
                 amount_min = ore.minable.count or 1,
@@ -79,8 +81,8 @@ local function createStackedOre(oreName)
             }}
             ore.minable.result=nil
             ore.minable.count=nil
-        else 
-            log("Error in createStackedOre() for " .. result.name)
+        else
+            log("Error in createStackedOre() for " .. ore.minable.result)
         end
     end
 
@@ -95,14 +97,14 @@ local function createStackedOre(oreName)
 
     -- the ore should never occur naturally
     ore.autoplace = nil
-    
+
     -- if the ore is infinite, increase the infinite_depletion_amount accordingly
-    if ore.infinite then 
+    if ore.infinite then
         ore.infinite_depletion_amount = (ore.infinite_depletion_amount or 1) * stackSize
     end
 
     -- option in the settings to adjust the graphics of ores
-    if getSettingValue("kyth-adjust-stage-counts") and ore.stage_counts then 
+    if getSettingValue("kyth-adjust-stage-counts") and ore.stage_counts then
         for i, stage in ipairs(ore.stage_counts) do
             ore.stage_counts[i] = math.ceil(stage / stackSize)
         end
@@ -124,13 +126,13 @@ local function createCompressedFluidResource(fluidName)
 
     -- replace the mining result with the high pressure version
     if fluidResource.minable.results then
-        
+
         if fluidResource.minable.results[1].name then
             fluidResource.minable.results[1].name = "high-pressure-" .. fluidResource.minable.results[1].name
         elseif fluidResource.minable.results[1][1] then
             fluidResource.minable.results[1][1] = "high-pressure-" .. fluidResource.minable.results[1][1]
         end
-    elseif ore.minable.result then        
+    elseif ore.minable.result then
         if fluidResource.minable.result.name then
             fluidResource.minable.result.name = "high-pressure-" .. fluidResource.minable.result.name
         elseif fluidResource.minable.result[1] then
@@ -145,9 +147,9 @@ local function createCompressedFluidResource(fluidName)
 
     -- the ResourceEntity should never occur naturally
     fluidResource.autoplace = nil
-    
+
     -- if the ResourceEntity is infinite, increase the infinite_depletion_amount accordingly
-    if fluidResource.infinite then 
+    if fluidResource.infinite then
         fluidResource.infinite_depletion_amount = (fluidResource.infinite_depletion_amount or 1) * getSettingValue("fluid-compression-rate")
     end
 
@@ -160,22 +162,22 @@ end
 local resourceTable = {}
 for _, resource in pairs(data.raw["resource"]) do
 
-    if resource.category == nil or resource.category == "basic-solid" or resource.category == "kr-quarry" then 
+    if resource.category == nil or resource.category == "basic-solid" or resource.category == "kr-quarry" then
 
         -- check if nil because in that case at the end of the data stage the value would be set to the default, which is "basic-solid"
         table.insert(resourceTable, createStackedOre(resource.name))
         log("Sucessfully created the ResourceEntity for the stacked ore version of " .. resource.name)
-    
+
     -- Support for Pressurized fluids
     elseif mods["CompressedFluids"] and (resource.category == "basic-fluid" or resource.category == "oil" or resource.category == "angels-fissure") then
-        -- check if a high pressure version fluid exists that corresponds to the mining result of a resource 
+        -- check if a high pressure version fluid exists that corresponds to the mining result of a resource
         if (resource.minable.result and (data.raw.fluid["high-pressure-" .. (resource.minable.result.name or resource.minable.result[1]) ] )) or
            (resource.minable.results and (data.raw.fluid["high-pressure-" .. (resource.minable.results[1].name or resource.minable.results[1][1]) ] )) then
 
             table.insert(resourceTable, createCompressedFluidResource(resource.name))
             log("Sucessfully created the ResourceEntity for the high pressure version of " .. resource.name)
         end
-    else 
+    else
         log("Skipping the resource " .. resource.name .. " because it is neither basic-solid, kr-quarry nor basic-fluid. Feel free to contact the mod author if you feel like the resource " .. resource.name .. " should be supported")
     end
 end
